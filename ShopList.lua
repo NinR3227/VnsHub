15bbb103b283bed5835406f83b40d094fd510126
NinR3227 <161265005+NinR3227@users.noreply.github.com>
2025-08-12 19:06:35 +0800

    Create ShopList.lua

---
+++ ShopList.lua
@@ -0,0 +1,16 @@
+-- ShopList.lua
+return {
+    Seeds = {
+        "Carrot", "Strawberry", "Blueberry", "Orange Tulip", "Tomato", "Corn", "Daffodil", "Watermelon", "Pumpkin",
+        "Apple", "Bamboo", "Coconut", "Cactus", "Dragon Fruit", "Mango", "Grape", "Mushroom", "Pepper", "Cacao",
+        "Beanstalk", "Ember Lily", "Sugar Apple", "Burning Bud", "Giant Pinecone", "Elder Strawberry"
+    },
+    Gears = {
+        "Watering Can", "Trading Ticket", "Trowel", "Recall Wrench", "Basic Sprinkle", "Advanced Sprinkler", "Medium Toy",
+        "Medium Treat", "Godly Sprinkler", "Magnifying Glas", "Master Sprinkler", "Cleaning Spray", "Favorit Tool",
+        "Harvest Tool", "Friendship Pot", "Grandmaster Sprinkle", "Levelup Lollipop"
+    },
+    Eggs = {
+        "Common", "Common Summer", "Rare Summer", "Mythical", "Paradise", "Bug"
+    }
+}

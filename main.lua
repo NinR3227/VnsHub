f633bbc2bd3c9d1d15047a0361289291c3325b62
NinR3227 <161265005+NinR3227@users.noreply.github.com>
2025-08-13 09:32:11 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,13 @@
 local ShopList = loadstring(game:HttpGet("https://raw.githubusercontent.com/NinR3227/VnsHub/main/shoplist.lua"))()
 local BuySeedStock = game:GetService("ReplicatedStorage").GameEvents:WaitForChild("BuySeedStock")
 
+local BUY_COUNT = 25 -- Number of Times to buy each seed
+
 for _, seed in ipairs(ShopList.Seeds) do
-    BuySeedStock:FireServer(seed)
-    wait(0.2)
+	for i = 1, BUY_COUNT do
+		BuySeedStock:FireSer(seed)
+		wait(0.1)
+	end
 end
 
-print("[✅ Bought All Seeds]")
+print ("[Bought All Seeds x" ..BUY_COUNT .. "]")

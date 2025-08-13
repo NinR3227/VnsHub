ba6aa92f225557d5ff1d425276b495048c07c75d
NinR3227 <161265005+NinR3227@users.noreply.github.com>
2025-08-13 09:47:55 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,24 @@
 local ShopList = loadstring(game:HttpGet("https://raw.githubusercontent.com/NinR3227/VnsHub/main/shoplist.lua"))()
-local BuySeedStock = game:GetService("ReplicatedStorage").GameEvents:WaitForChild("BuySeedStock")
 
-local BUY_COUNT = 25 -- Number of Times to buy each seed
+local ShopRemotes = {
+	Seeds = game:GetService("ReplicatedStorage").GameEvents:WaitForChild("BuySeedStock"),
+	Gears = game:GetService("ReplicatedStorage").GameEvents:WaitForChild("BuyGearStock"),
+	Eggs = game:GetService("ReplicatedStorage").GameEvents:WaitForChild("BuyEggStock")
+}
 
-for _, seed in ipairs(ShopList.Seeds) do
-	for i = 1, BUY_COUNT do
-		BuySeedStock:FireServer(seed)
-		wait(0.1)
+local BUY_COUNT = 25 -- Number of Times to buy each items
+
+for category, itemList in pairs(ShopList) do
+	local remote = ShopRemote[category]
+	if remote then
+		for _, item in ipairs(itemList) do
+			for i = 1, BUY_COUNT do
+				remote:Fireserver(item)
+				wait(0.001)
+			end
+		end
+		print("[Bought All " .. category .. "]")
+	else
+		warn("[No RemoteEvent found for category: " .. category .. "]")
 	end
 end
-
-print ("[Bought All Seeds x" .. BUY_COUNT .. "]")

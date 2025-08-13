25d890f9fc7b0eda304c74851992fbea90a86a17
NinR3227 <161265005+NinR3227@users.noreply.github.com>
2025-08-13 09:24:48 +0800

    Automaiton Tsting

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local ShopList = loadstring(readfile("shoplist.lua"))()
+local ShopList = loadstring(game:HttpGet("https://raw.githubusercontent.com/NinR3227/VnsHub/main/shoplist.lua"))()
 local BuySeedStock = game:GetService("ReplicatedStorage").GameEvents:WaitForChild("BuySeedStock")
 
 for _, seed in ipairs(ShopList.Seeds) do
